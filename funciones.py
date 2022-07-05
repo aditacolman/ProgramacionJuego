3fe00badc42b222e92d9a7612e39b8f4bf16bb2b
adi <acolman@sagradoalcal.edu.ar>
2022-07-05 16:53:59 -0300

    Organizar el c

--- funciones.py
+++ funciones.py
@@ -1,16 +1,70 @@
-def AparecerVP(lista_ventanas):
-    ventanaSesion = lista_ventanas[0]
-    ventanaPrincipal = lista_ventanas[1]
-    ventanaSesion.withdraw()
-    ventanaPrincipal.deiconify()
-    ventanaPrincipal.title("")
+from tkinter import *
+from functools import partial
 
-def AparecerVJ(lista_ventanas):
-    ventana
-    pass
 
-def Eleccion():
-    print("Funcionó")
-    pass
+def usuario_contrasena():
+    nombre= nombreUsuario.get()
+    contrasena= contrasenaUsuario.get()
+    registrar_usuario(nombre, contrasena)
+
+
+
+class ventana:
+    indice = 0
+    def crear_ventana_sesion(self):
+        ventanaSesion = Tk()
+        ventanaSesion.title("Login Usuario")
+        mainFrame= Frame(ventanaSesion)
+        mainFrame.pack()
+        titulo= Label(mainFrame, text="Login de Usuario", font=("Arial 24"))
+        titulo.grid(column=0, row=0, padx=10, pady=10, columnspan=2)
+        mainFrame.config(width=400, height=200) #bg= "#1AA1EE")
+        iniciarSesion= Button(mainFrame, command=self.siguiente, text="Iniciar sesión")
+        iniciarSesion.grid(column=1, row=3, ipadx=5, ipady=5, padx=10, pady=10)
+        registrarUsuario= Button(mainFrame, command= usuario_contrasena, text="Registrar")
+        registrarUsuario.grid(column=0, row=3, ipadx=5, ipady=5, padx=10, pady=10)
+        nombreLabel= Label(mainFrame, text="Nombre: ")
+        nombreLabel.grid(column=0, row=1)
+        contrasenaLabel = Label(mainFrame, text="Contraseña: ")
+        contrasenaLabel.grid(column=0, row=2)
+        nombreUsuario= StringVar()
+        nombreEntry = Entry(mainFrame, textvariable=nombreUsuario)
+        nombreEntry.grid(column=1, row=1)
+        contrasenaUsuario= StringVar()
+        contrasenaEntry = Entry(mainFrame, textvariable=contrasenaUsuario, show="*")
+        contrasenaEntry.grid(column=1, row=2)
+        return ventanaSesion
+
+    def crear_ventana_juego(self):
+        ventanaJuego= Tk()
+        ventanaJuego.title("")
+        ventanaJuego.withdraw()
+        return ventanaJuego
+
+    def crear_ventana_eleccion(self):
+        ventanaEleccion= Tk()
+        ventanaEleccion.title("")
+        ventanaEleccion.withdraw()
+        elegirPeli= Button(ventanaEleccion,command=self.siguiente, text="Película").place(x=100, y=100)
+        elegirSerie= Button(ventanaEleccion,command=self.siguiente, text="Serie").place(x=40, y=100)
+        return ventanaEleccion
+    
+    def siguiente(self):
+        self.windows[self.indice].withdraw()
+        self.indice += 1
+        self.windows[self.indice].deiconify()
+        
+ 
+    
+    
+    tipos = [crear_ventana_sesion, crear_ventana_eleccion, crear_ventana_juego]
 
     
+    def __init__(self):
+        self.vs = self.tipos[0](self)
+        self.ve = self.tipos[1](self)
+        self.vj = self.tipos[2](self)
+        #self.ve.withdraw()
+        #self.vj.withdraw()
+        self.windows = [self.vs, self.ve, self.vj]
+        
--- pruebainiciodesesion.py
+++ pruebainiciodesesion.py
@@ -1,63 +1,11 @@
-import requests
-import json
-import random
-from tkinter import *
 from pruebabd import registrar_usuario
 import funciones
-from functools import partial
-
-#Ventana de sesión
-ventanaSesion = Tk()
-ventanaSesion.title("Login Usuario")
-
-#mainFrame de sesión
-mainFrame= Frame(ventanaSesion)
-mainFrame.pack()
-mainFrame.config(width=400, height=200) #bg= "#1AA1EE")
-
-#Textos y titulos de sesión
-titulo= Label(mainFrame, text="Login de Usuario", font=("Arial 24"))
-titulo.grid(column=0, row=0, padx=10, pady=10, columnspan=2)
-
-#Label's de sesión
-nombreLabel= Label(mainFrame, text="Nombre: ")
-nombreLabel.grid(column=0, row=1)
-contrasenaLabel = Label(mainFrame, text="Contraseña: ")
-contrasenaLabel.grid(column=0, row=2)
-
-#Entradas de texto
-nombreUsuario= StringVar()
-nombreEntry = Entry(mainFrame, textvariable=nombreUsuario)
-nombreEntry.grid(column=1, row=1)
-contrasenaUsuario= StringVar()
-contrasenaEntry = Entry(mainFrame, textvariable=contrasenaUsuario, show="*")
-contrasenaEntry.grid(column=1, row=2)
-
-def usuario_contrasena():
-    nombre= nombreUsuario.get()
-    contrasena= contrasenaUsuario.get()
-    registrar_usuario(nombre, contrasena)
 
 
-#Ventana elección
-ventanaEleccion= Tk()
-ventanaEleccion.title("")
-ventanaEleccion.withdraw()
-elegirPeli= Button(ventanaEleccion,command=funciones.Eleccion, text="Película").place(x=100, y=100)
-elegirSerie= Button(ventanaEleccion,command=funciones.Eleccion, text="Serie").place(x=40, y=100)
 
-#Ventana de juego
-ventanaJuego= Tk()
-ventanaJuego.title("")
-ventanaJuego.withdraw()
+if __name__ == "__main__":
+    
+    inicio = funciones.ventana()
+    inicio.vs.mainloop()
 
-lista_ventanas = [ventanaSesion, ventanaEleccion, ventanaJuego]
-
-#Botones de sesión
-iniciarSesion= Button(mainFrame, command= partial(funciones.AparecerVP, lista_ventanas), text="Iniciar sesión")
-iniciarSesion.grid(column=1, row=3, ipadx=5, ipady=5, padx=10, pady=10)
-registrarUsuario= Button(mainFrame, command= usuario_contrasena, text="Registrar")
-registrarUsuario.grid(column=0, row=3, ipadx=5, ipady=5, padx=10, pady=10)       
-
-
-ventanaSesion.mainloop()
+    
