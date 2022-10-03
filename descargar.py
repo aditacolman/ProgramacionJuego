27b10f92497486099fc0acf9e614643129611ce5
iarachamorro5 <iara.chamorro@sagradoalcal.edu.ar>
2022-10-03 15:10:28 -0300

    descargar videos

--- descargar.py
+++ descargar.py
@@ -1,24 +1,10 @@
 from pytube import YouTube
 
-class descargar(App):
-    
-    def __init__(self):
-        self.url= "https://www.youtube.com/watch?v=kLw1UrDqC9Q"
-        
-    def descargar_peli(self):
-        yt = YouTube(self.url)
-        yt.streams
-        resp = yt.streams.filter(file_extension='mp4')
-        len(resp)
-        resp[0].download()
-        self.titulo=resp[0].title
-        print("Video descargado")
-        return self.titulo
-    
-if __name__ == '__main__':
-
-    descargar().run()
-
+url = "https://www.youtube.com/watch?v=kLw1UrDqC9Q"
+video = YouTube(url)
+video_streams = video.streams.filter(file_extension ='mp4').get_by_itag(22)
+video_streams.download(filename = "1")
+print(video_streams)
 '''
 class R:
     def __init__(self):
